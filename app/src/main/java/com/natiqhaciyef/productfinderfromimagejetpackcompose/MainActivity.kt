164a45767680581527ed573cf1c8f6f2eb8cf816
Natiq <natiq00h2272@gmail.com>
2023-01-21 19:56:30 +0400

    ProductDetectorScreen.kt created

--- app/src/main/java/com/natiqhaciyef/productfinderfromimagejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/natiqhaciyef/productfinderfromimagejetpackcompose/MainActivity.kt
@@ -74,7 +74,7 @@ fun AppTheme(content: @Composable () -> Unit) {
             color = MaterialTheme.colors.background
         ) {
             content()
-            ProductDetectorScreen()
+            ProductLabelingScreen()
         }
     }
 }
