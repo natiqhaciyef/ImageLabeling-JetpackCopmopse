f0ed0beb2b104da08e259942042b7b669d74b010
Natiq <natiq00h2272@gmail.com>
2023-01-21 19:30:06 +0400

    Labeling screen created

--- app/src/main/java/com/natiqhaciyef/productfinderfromimagejetpackcompose/view/ProductDetectorScreen.kt
+++ app/src/main/java/com/natiqhaciyef/productfinderfromimagejetpackcompose/view/ProductDetectorScreen.kt
@@ -15,10 +15,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Colors
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -29,7 +29,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
@@ -42,16 +46,21 @@ import com.google.mlkit.vision.label.ImageLabeling
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.google.mlkit.vision.objects.DetectedObject
 import com.google.mlkit.vision.objects.defaults.PredefinedCategory
+import com.natiqhaciyef.productfinderfromimagejetpackcompose.R
 import com.natiqhaciyef.productfinderfromimagejetpackcompose.ui.theme.Brown
 import com.natiqhaciyef.productfinderfromimagejetpackcompose.ui.theme.DarkBlue
 import com.natiqhaciyef.productfinderfromimagejetpackcompose.ui.theme.ExtraLightBlue
 import com.natiqhaciyef.productfinderfromimagejetpackcompose.ui.theme.LightBrown
 import java.io.IOException
 
+@Preview
 @Composable
 fun ProductDetectorScreen() {
     val detectedObject = remember {
         mutableStateOf(mutableListOf<DetectedObject>())
+    }
+    val labelDetector = remember {
+        mutableStateOf(mutableListOf<ImageLabel>())
     }
     val imageData = remember { mutableStateOf<Bitmap?>(null) }
     val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
@@ -61,24 +70,42 @@ fun ProductDetectorScreen() {
 
 
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.customBackgroundColor)
     ) {
 
-        imageData.value?.let { image ->
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            text = "Select image and show result",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = MaterialTheme.colors.reverseThemeModeColor
+        )
+        Spacer(modifier = Modifier.height(60.dp))
+
+        if (imageData.value != null) {
             Image(
                 modifier = Modifier
-                    .width(300.dp)
-                    .height(200.dp),
-                bitmap = image.asImageBitmap(),
+                    .fillMaxWidth()
+                    .height(300.dp),
+                bitmap = imageData.value!!.asImageBitmap(),
+                contentDescription = "Captured image",
+                contentScale = ContentScale.Fit
+            )
+        } else {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                painter = painterResource(id = R.drawable.select_pircture_image),
                 contentDescription = "Captured image",
                 contentScale = ContentScale.Fit
             )
         }
-        Spacer(modifier = Modifier.height(50.dp))
+        Spacer(modifier = Modifier.height(40.dp))
 
         Button(
             onClick = {
@@ -99,12 +126,75 @@ fun ProductDetectorScreen() {
             },
             modifier = Modifier
                 .width(200.dp)
-                .height(50.dp)
+                .height(50.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.customButtonColor
+            )
         ) {
-            Text(text = "Click for permission")
+            Text(
+                text = "Select image",
+                fontSize = 18.sp,
+                color = MaterialTheme.colors.reverseThemeModeColor
+            )
+        }
+        objectDetection(imageData, detectedObject)
+        labelDetector(labelDetector, imageData)
+
+        if (detectedObject.value != null) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(top = 30.dp),
+                shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+            ) {
+                LazyColumn(
+                    modifier = Modifier.padding(top = 15.dp)
+                ) {
+                    items(items = labelDetector.value) { label ->
+                        DetectedObjectCard(label = label)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DetectedObjectCard(label: ImageLabel) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .padding(horizontal = 10.dp, vertical = 7.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp,
+        backgroundColor = MaterialTheme.colors.themeModeColor
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                Text(text = "Item category: ", color = MaterialTheme.colors.reverseThemeModeColor)
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = "${label.text}",
+                    color = MaterialTheme.colors.reverseThemeModeColor
+                )
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Row {
+                Text(text = "Confidence: ", color = MaterialTheme.colors.reverseThemeModeColor)
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = "${label.confidence}",
+                    color = MaterialTheme.colors.reverseThemeModeColor
+                )
+            }
         }
 
-        objectDetection(imageData, detectedObject)
     }
 }
 
