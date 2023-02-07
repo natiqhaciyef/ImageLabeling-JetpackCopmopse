b3494fb67cffc86a2f911a85cd54f1d76da81445
Natiq <natiq00h2272@gmail.com>
2023-02-07 16:24:22 +0400

    ML kit HomeScreen created successfully

--- app/src/main/java/com/natiqhaciyef/productfinderfromimagejetpackcompose/view/HomeScreen.kt
+++ app/src/main/java/com/natiqhaciyef/productfinderfromimagejetpackcompose/view/HomeScreen.kt
@@ -8,15 +8,20 @@ import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Beenhere
+import androidx.compose.material.icons.filled.GTranslate
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import com.natiqhaciyef.productfinderfromimagejetpackcompose.ui.theme.*
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,7 +31,7 @@ import com.natiqhaciyef.productfinderfromimagejetpackcompose.R
 
 @Preview
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.AppHomeBackgroundColor
@@ -45,9 +50,9 @@ fun HomeScreen(){
 
             Card(
                 shape = RoundedCornerShape(12.dp),
-                modifier = Modifier.padding(20.dp)
+                modifier = Modifier.padding(top = 40.dp, start = 20.dp, end = 20.dp, bottom = 20.dp)
             ) {
-                 Categories()
+                Categories()
             }
         }
     }
@@ -56,9 +61,11 @@ fun HomeScreen(){
 
 @Preview
 @Composable
-fun Categories(){
+fun Categories() {
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -67,53 +74,93 @@ fun Categories(){
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Spacer(modifier = Modifier.width(30.dp))
+            Spacer(modifier = Modifier.width(10.dp))
             Card(
                 shape = RoundedCornerShape(15.dp),
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(120.dp),
+                elevation = 5.dp
             ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(5.dp))
 
+                    Icon(
+                        imageVector = Icons.Default.GTranslate,
+                        contentDescription = "Translator",
+                        modifier = Modifier.size(50.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+                    
+                    Text(
+                        text = "Translate",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                        )
+                }
             }
-            Spacer(modifier = Modifier.width(30.dp))
-
-            Spacer(modifier = Modifier.width(30.dp))
+            Spacer(modifier = Modifier.width(40.dp))
             Card(
                 shape = RoundedCornerShape(15.dp),
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(120.dp),
+                elevation = 5.dp
             ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(5.dp))
 
+                    Icon(
+                        imageVector = Icons.Default.Beenhere,
+                        contentDescription = "Image Labeling",
+                        modifier = Modifier.size(50.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Text(
+                        text = "Translate",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                }
             }
-            Spacer(modifier = Modifier.width(30.dp))
+            Spacer(modifier = Modifier.width(10.dp))
         }
 
         Spacer(modifier = Modifier.height(10.dp))
-        
+
         Row(
             modifier = Modifier.padding(10.dp),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Spacer(modifier = Modifier.width(30.dp))
+            Spacer(modifier = Modifier.width(10.dp))
             Card(
                 shape = RoundedCornerShape(15.dp),
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(120.dp),
+                elevation = 5.dp
             ) {
 
             }
-            Spacer(modifier = Modifier.width(30.dp))
-
-            Spacer(modifier = Modifier.width(30.dp))
+            Spacer(modifier = Modifier.width(40.dp))
             Card(
                 shape = RoundedCornerShape(15.dp),
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(120.dp),
+                elevation = 5.dp
             ) {
 
             }
-            Spacer(modifier = Modifier.width(30.dp))
+            Spacer(modifier = Modifier.width(10.dp))
         }
     }
 }
